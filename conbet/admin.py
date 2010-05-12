8adac839bb81216a341e5ef6e5778bb9e696e297
Javier Lopez <jlopez@fi.upm.es>
2010-05-12 09:43:46 +0200

    Added admin application

---
+++ conbet/admin.py
@@ -0,0 +1,9 @@
+from models import Team, Group, Match, Round, GroupQualification, Result
+from django.contrib import admin
+
+admin.site.register(Team)
+admin.site.register(Group)
+admin.site.register(Match)
+admin.site.register(Round)
+admin.site.register(GroupQualification)
+admin.site.register(Result)
