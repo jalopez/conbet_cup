374c3014dd06fab6dbeef26e64b1cac6c1a42d6c
Javier Lopez <jlopez@fi.upm.es>
2010-05-14 12:59:54 +0200

    Some views functionality, but not ended

--- conbet/views.py
+++ conbet/views.py
@@ -1,7 +1,12 @@
+import json
+
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseServerError
 from django.shortcuts import get_list_or_404, get_object_or_404, render_to_response
+from django.contrib.auth.models import User
+
+from conbet.models import Match, Bet, GroupMatch, Round
 
 @login_required
 def index(request):
@@ -16,12 +21,39 @@ def ranking(request):
 
 @login_required
 def edit_bet(request):
-    raise Http404
+    if request.method == 'POST':
+        update_bet(request)
+    return show_user_bet(request.user, True) 
 
 @login_required
 def bet(request, username):
-    raise Http404
+    user = get_object_or_404(User, username=username)
+    return show_user_bet(user, False)
 
 @login_required
 def results(request):
     raise Http404
+
+### Aux functions
+
+def show_user_bet(user, editable):
+    bets = Bet.objects.filter(owner=user)
+    group_matches = GroupMatch.objects.all().order_by('group__name', 'date')
+    rounds = Round.objects.all().order_by('stage', 'order')
+    return render_to_response('bet.html', {'bets': bets, 'group_matches':
+         group_matches, 'round_matches': rounds, 'editable': editable})
+
+def update_bet(request):
+    try:
+        bet_info = json.loads(request.POST.get('bets'))
+        for (match_id, match_info) in bet_info.items():
+            match = Match.objects.get(id=match_id)
+            bet = Bet.objects.get(owner=request.user, match=match)
+            if not bet:
+                bet = Bet(owner=request.user, match=match)
+            bet.home_goals = match_info['home_goals']
+            bet.visitor_goals = match_info['visitor_goals']
+            bet.winner = match_info['winner']
+            bet.save()        
+    except e:
+        raise HttpResponseServerError("Something happened %s" % e) 
